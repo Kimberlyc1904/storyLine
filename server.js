ecc51ff54604f82c132d38267fba689f7df78594
CJacobs <cjacobs3971@gmail.com>
2023-07-13 18:59:32 -0700

    test changes

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const session = require('express-session');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const exphbs = require('express-handlebars');
 const path = require('path');
+const controllers = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,26 +15,47 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+// Set up session middleware
+app.use(session(sess));
 
 // Handlebars
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Session
-app.use(
-  session({
-    secret: 'your-secret-session-key',
-    resave: false,
-    saveUninitialized: true
-  })
-);
+// app.use(
+//   session({
+//     secret: 'your-secret-session-key',
+//     resave: false,
+//     saveUninitialized: true
+//   })
+// );
+
+// Set up session options
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+
+
+// Middleware for parsing request bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
-const controllers = require('./controllers');
+
 app.use(controllers);
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`Server listening on http://localhost:${PORT}`);
+// });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening on port', PORT));
 });
-
